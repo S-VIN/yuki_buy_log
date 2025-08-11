0c5c623d58725e3646c5cd7f0e0625a757638ba2
S-VIN <32909412+S-VIN@users.noreply.github.com>
2025-08-11 10:35:38 +0300

    Enable CORS and upgrade dependencies

--- server/main.go
+++ server/main.go
@@ -31,5 +31,18 @@ func main() {
 	mux.HandleFunc("/login", srv.loginHandler)
 
 	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", enableCORS(mux)))
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
